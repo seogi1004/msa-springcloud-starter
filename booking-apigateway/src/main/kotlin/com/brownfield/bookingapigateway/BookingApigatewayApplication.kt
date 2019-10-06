b63f9e107fd59198bba000692245252de62e28d0
alvin <seogi777@gmail.com>
2019-10-06 11:02:05 +0900

    API 게이트웨이 적용 완료

--- booking-apigateway/src/main/kotlin/com/brownfield/bookingapigateway/BookingApigatewayApplication.kt
+++ booking-apigateway/src/main/kotlin/com/brownfield/bookingapigateway/BookingApigatewayApplication.kt
@@ -2,7 +2,11 @@ package com.brownfield.bookingapigateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 class BookingApigatewayApplication
 
